e764f4d081fb151862d9ffbe1e18180cdb00aa71
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-08-29 15:17:02 -0700

    Add a working Python kerchunk roundtrip test

--- test/python_kerchunk.jl
+++ test/python_kerchunk.jl
@@ -1,19 +1,61 @@
 using CondaPkg, PythonCall
 
-using Test
+using FSSpec, Zarr, JSON3
 
 const xr = pyimport("xarray")
 const fsspec = pyimport("fsspec")
-# const kerchunk = pyimport("kerchunk")
+# You can't import kerchunk.hdf because importing h5py introduces a version of libhdf5 that is incompatible with any extant netcdf4_jll.
 
-using FSSpec, Zarr, JSON3
 
-json_file_path = "https://raw.githubusercontent.com/lsterzinger/2022-esip-kerchunk-tutorial/main/example_jsons/individual/OR_ABI-L2-SSTF-M6_G16_s20202100000205_e20202100059513_c20202100105456.json"
+using Rasters, NCDatasets, Dates
 
-st = FSStore("reference://"; fo = json_file_path)
+using Test
 
-_zarr = Zarr.zopen(st; consolidated = false)
+@testset "Reading a Kerchunked NetCDF file" begin
 
-using YAXArrays
+# First, we create a NetCDF dataset:
 
-dataset = YAXArrays.open_dataset(_zarr)
+ras = Raster(rand(LinRange(0, 10, 100), X(1:100), Y(5:150), Ti(DateTime("2000-01-31"):Month(1):DateTime("2001-01-31"))))
+
+write("test.nc", ras; source = :netcdf, force = true)
+@test Raster("test.nc") == ras # test read-write roundtrip
+
+# Create a Kerchunk catalog.
+# The reason I do this by `run` is because the hdf5 C library versions used by 
+# Julia and Python are fundamentally incompatible, so we can't use the same process for both.
+CondaPkg.withenv() do
+    run(```
+python -c "
+import kerchunk.hdf as hdf; import os; import ujson
+h5chunks = hdf.SingleHdf5ToZarr('test.nc', inline_threshold=300)
+with open('test.json', 'w') as f:
+    f.write(ujson.dumps(h5chunks.translate())) 
+"
+    ```)
+end
+
+pykerchunkcatalog = JSON3.read(read("test.json", String)) |> FSSpec._recursive_pyconvert 
+
+st = FSSpec.FSStore("reference://"; fo = "test.json")
+st2 = FSSpec.FSStore("reference://"; fo = pykerchunkcatalog)
+#=
+# explore why fsspec might be causing problems
+fs, = fsspec.core.url_to_fs("s3://its-live-data/datacubes/v2/N00E020/ITS_LIVE_vel_EPSG32735_G0120_X750000_Y10050000.zarr")
+fs2, = fsspec.core.url_to_fs("reference://"; fo = pykerchunkcatalog)
+st.mapper.dirfs.ls("/")
+=#
+
+# ds = xr.open_dataset("reference://", engine="zarr", backend_kwargs={"consolidated": False, "storage_options": {"fo" : h5chunks.translate()}})
+
+ds = Zarr.zopen(st; consolidated = false)
+
+ya = YAXArrays.open_dataset(ds)
+
+@test all(map(==, ya["unnamed"] |> collect, ras |> collect)) # if not, this goes to YAXArrays 
+
+ds = Zarr.zopen(st2; consolidated = false)
+
+ya = YAXArrays.open_dataset(ds)
+
+@test all(map(==, ya["unnamed"] |> collect, ras |> collect)) # if not, this goes to YAXArrays
+end
