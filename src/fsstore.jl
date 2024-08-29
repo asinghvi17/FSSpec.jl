dd062803da6749baa2358a7d1f8c7d2b234c3c4c
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-08-29 15:17:30 -0700

    add a selected "dirfs" to FSStore to reduce headache

--- src/fsstore.jl
+++ src/fsstore.jl
@@ -27,21 +27,29 @@ If it is a Dict, it may not contain any values that cannot be translated directl
 """
 struct FSStore <: Zarr.AbstractStore
     url::URIs.URI
+    dirfs::Py
     mapper::Py
 end
 
 function Base.readdir(s::FSStore, p::String)
-    return pyconvert(Vector{String}, s.mapper.dirfs.ls(p, detail=false))
+    return pyconvert(Vector{String}, s.mapper.fs.ls(p, detail=false))
 end
 
 function Base.stat(s::FSStore, p::String)
-    return pyconvert(Py, s.mapper.dirfs.info(p))
+    return pyconvert(Py, s.dirfs.info(p))
 end
 
 function FSStore(url::String; storage_options...)
     fs, = fsspec[].core.url_to_fs(url; storage_options...)
-    mapper = fs.get_mapper(url)
-    return FSStore(URIs.URI(url), mapper)
+    # We have to handle reference:// separately, since passing 
+    # that as a URL to `fs.get_mapper` breaks it.
+    mapper, dirfs = if url == "reference://"
+        fs.get_mapper(""), fs
+    else
+        _m = fs.get_mapper(url)
+        _m, _m.dirfs
+    end
+    return FSStore(URIs.URI(url), dirfs, mapper)
 end
 
 function Base.getindex(s::FSStore, k::String)
@@ -49,7 +57,7 @@ function Base.getindex(s::FSStore, k::String)
 end
 
 function Zarr.storagesize(s::FSStore, p::String)
-    return pyconvert(Int, s.mapper.fs.du(p; total = true))
+    return pyconvert(Int, s.dirfs.du(p; total = true))
 end
 
 function Zarr.read_items!(s::FSStore, c::AbstractChannel, p, i)
@@ -62,16 +70,16 @@ function Zarr.read_items!(s::FSStore, c::AbstractChannel, p, i)
 end
 
 function Zarr.isinitialized(s::FSStore, p)
-    return pyconvert(Bool, s.mapper.fs.exists(p))
+    return pyconvert(Bool, s.dirfs.exists(p))
 end
 
 function Zarr.isinitialized(s::FSStore, p, i)
-    return pyconvert(Bool, s.mapper.fs.exists("$p/$i"))
+    return pyconvert(Bool, s.dirfs.exists("$p/$i"))
 end
 
 function listdir(s::FSStore, p; nometa=false) 
     try
-        listing = pyconvert(Vector{String}, s.mapper.dirfs.ls(p, detail=false))
+        listing = pyconvert(Vector{String}, s.dirfs.ls(p, detail=false))
         if nometa
           filter!(!startswith("."), listing)
         end
@@ -83,11 +91,11 @@ end
 
 
 Zarr.subdirs(s::FSStore, p) = filter(listdir(s, p; nometa = true)) do path
-    pyconvert(Bool, s.mapper.dirfs.isdir(path))
+    pyconvert(Bool, s.dirfs.isdir(path))
 end
 
 Zarr.subkeys(s::FSStore, p) = filter(listdir(s, p; nometa = true)) do path
-    pyconvert(Bool, s.mapper.dirfs.isfile(path))
+    pyconvert(Bool, s.dirfs.isfile(path))
 end
 
 
@@ -95,6 +103,8 @@ end
 Zarr.is_zarray(s::FSStore, p) = ((normpath(p) in ("/", ".")) ? ".zarray" : normpath("$p/.zarray")) in listdir(s, p, nometa=false) # TODO: this is where relative file utils could be SUPER helpful.
 Zarr.is_zgroup(s::FSStore, p) = ((normpath(p) in ("/", ".")) ? ".zgroup" : normpath("$p/.zgroup")) in listdir(s, p, nometa=false)
 
+# TODO: is it a requirement of the Zarr spec that each directory have a .zattrs file?  If not, should that be checked in getattrs?  This was an annoying bug...
+Zarr.getattrs(s::FSStore, p) = pyconvert(Bool, s.dirfs.exists(normpath(p) in ("/", ".") ? ".zattrs" : "$p/.zattrs")) ? Zarr.JSON.parse(String(s[normpath(p) in ("/", ".") ? ".zattrs" : "$p/.zattrs"])) : Dict{String, Any}()
 
 struct PyConcurrentRead end
 
